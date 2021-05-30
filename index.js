3372e16ff400495eed0297d7bd3ff9430b03b0b9
rahulgupta655 <83936424+rahulgupta655@users.noreply.github.com>
2021-05-30 07:19:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0";
+}
+
+function toggle() {
+  document.getElementById("hidden_div").style.display = "block";
+  document.getElementById("load_btn").style.display = "none";
+}
+
+function NextPage() {
+  document.getElementById("page2").style.display = "block";
+}
